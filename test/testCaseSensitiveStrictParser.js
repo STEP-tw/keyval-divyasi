405af25fea6df863ed4ee17276f8153db14f4a39
Divya Singh <ds492015@gmail.com>
2018-01-08 15:39:50 +0530

    reuired chai

--- test/testCaseSensitiveStrictParser.js
+++ test/testCaseSensitiveStrictParser.js
@@ -1,17 +1,17 @@
 const src=function(filePath){return "../src/"+filePath};
 
-const assert=require('assert');
+//const assert=require('assert');
+const assert=require('chai').assert;
 const Parsed=require(src('parsed.js'));
 const StrictParser=require(src('index.js')).StrictParser;
 
-
 describe("strict parser that is case insensitive",function(){
   it("should parse when specified keys are in lower case and actual is not",function(){
-    let kvParser=new StrictParser(["name"],false  );
+    let kvParser=new StrictParser(["name"],false);
     // false indicates that case sensitive is false. By default it is true
     let expected=new Parsed();
-    expected["Name"]="jayanth";
-    let parsed=kvParser.parse("Name=jayanth");
+    expected["NAME"]="jayanth";
+    let parsed=kvParser.parse("NAME=jayanth");
     assert.deepEqual(parsed,expected);
   });
 });
